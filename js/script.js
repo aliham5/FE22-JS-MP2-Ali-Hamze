ac9f5e55fc29a6ad687e7ec1bbccfd9856a73fc5
aliham5 <106738417+aliham5@users.noreply.github.com>
2022-12-13 10:08:48 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let randomNumber ;
+let randomNumber;
 
-const computerchoice  = ["STEN", "SAX", "PÅSE"];
+const computerchoice = ["STEN", "SAX", "PÅSE"];
 let ichoose;
 let myname;
 let computerscore = 0;
@@ -13,42 +13,42 @@ const namn = document.querySelector(".myname");
 const message = document.querySelector(".computerchoice");
 const score = document.querySelector(".myscore");
 const text = document.querySelector(".ichoose");
-const Sten = document.querySelector(".Sten");
-const Sax = document.querySelector(".Sax");
-const Påse = document.querySelector(".Påse");
+const sten = document.querySelector(".Sten");
+const sax = document.querySelector(".Sax");
+const påse = document.querySelector(".Påse");
 const result = document.querySelector(".result");
 const cs = document.querySelector(".computerscore");
 const knapp = document.querySelector(".restart")
 
 
-Sten.addEventListener("click", function () {
+sten.addEventListener("click", function () {
     ichoose = "STEN";
     console.log(ichoose);
     startGame();
-    text.innerHTML =  (myname) +  " valde: STEN";
+    text.innerHTML = (myname) + " valde: STEN";
     point();
-    
 
-    
+
+
 });
 
-Sax.addEventListener("click", function () {
+sax.addEventListener("click", function () {
     ichoose = "SAX"
     console.log(ichoose)
     startGame();
-    text.innerHTML = (myname) +  " valde: SAX"
+    text.innerHTML = (myname) + " valde: SAX"
     point();
-   
+
 
 });
 
-Påse.addEventListener("click", function () {
+påse.addEventListener("click", function () {
     ichoose = "PÅSE"
     console.log(ichoose)
     startGame();
-    text.innerHTML =  (myname) +  " valde: PÅSE"
+    text.innerHTML = (myname) + " valde: PÅSE"
     point();
-    
+
 
 
 });
@@ -57,7 +57,8 @@ btn.addEventListener("click", function () {
     myname = document.querySelector("input").value
     console.log(document.querySelector("input").value);
     namn.innerHTML = (myname);
-    
+    event.preventDefault();
+
 
 
 });
@@ -66,89 +67,95 @@ btn.addEventListener("click", function () {
 
 
 function startGame() {
-    randomNumber = Math.round(Math.random()* 2);
+    randomNumber = Math.round(Math.random() * 2);
     console.log(computerchoice[randomNumber]);
     message.innerHTML = "Motståndarens val: " + (computerchoice[randomNumber]);
-    
+
 }
 
-function point(){
-    if(ichoose == "STEN" && computerchoice[randomNumber] == "STEN"){
+function point() {
+    if (ichoose == "STEN" && computerchoice[randomNumber] == "STEN") {
         console.log("LIKA")
-    
-        
+
+
 
     }
-    else if(ichoose == "STEN" && computerchoice[randomNumber] == "SAX"){
+    else if (ichoose == "STEN" && computerchoice[randomNumber] == "SAX") {
         console.log(ichoose = "1 poäng")
         myscore += 1;
         score.innerHTML = myname + " har " + myscore + " antal poäng";
-      
+
     }
-    else if(ichoose == "STEN" && computerchoice[randomNumber] == "PÅSE"){
+    else if (ichoose == "STEN" && computerchoice[randomNumber] == "PÅSE") {
         console.log("Motståndare: 1 poäng")
         computerscore += 1;
         cs.innerHTML = "Motståndare har " + computerscore + " antal poäng";
-       
-        
+
+
     }
-    else if(ichoose == "SAX" && computerchoice[randomNumber] == "SAX"){
+    else if (ichoose == "SAX" && computerchoice[randomNumber] == "SAX") {
         console.log("LIKA")
-       
-       
-    
+
+
+
     }
-    else if(ichoose == "SAX" && computerchoice[randomNumber] == "STEN"){
+    else if (ichoose == "SAX" && computerchoice[randomNumber] == "STEN") {
         console.log("Motståndaren: 1 poäng")
         computerscore += 1;
         cs.innerHTML = "Motståndare har " + computerscore + " antal poäng";
-       
-    
+
+
     }
-    else if(ichoose == "SAX" && computerchoice[randomNumber] == "PÅSE"){
+    else if (ichoose == "SAX" && computerchoice[randomNumber] == "PÅSE") {
         console.log(ichoose = "1 poäng")
         myscore += 1
         score.innerHTML = myname + " har " + myscore + " antal poäng";
-       
-        
+
+
     }
-    else if(ichoose == "PÅSE" && computerchoice[randomNumber] == "STEN"){
+    else if (ichoose == "PÅSE" && computerchoice[randomNumber] == "STEN") {
         console.log(ichoose = "1 poäng")
         myscore += 1
         score.innerHTML = myname + " har " + myscore + " antal poäng";
-        
-        
+
+
     }
-    else if(ichoose == "PÅSE" && computerchoice[randomNumber] == "SAX"){
+    else if (ichoose == "PÅSE" && computerchoice[randomNumber] == "SAX") {
         console.log("Motståndaren: 1 poäng")
         computerscore += 1;
         cs.innerHTML = "Motståndare har " + computerscore + " antal poäng";
-        
+
     }
-    else if(ichoose == "PÅSE" && computerchoice[randomNumber] == "PÅSE"){
+    else if (ichoose == "PÅSE" && computerchoice[randomNumber] == "PÅSE") {
         console.log("LIKA")
-       
-    }
-win();
 
+    }
+
+    setTimeout(win, 200);
 }
 
 
-function win(){
-    
-    if(myscore === 3){
-        alert("DU VANN")
-        
+
+
+
+function win() {
+
+    if (myscore === 3) {
+        alert("DU VANN");
+
+
     }
-    else if(computerscore === 3){
-        alert("MOTSTÅNDAREN VANN")
+    else if (computerscore === 3) {
+        alert("MOTSTÅNDAREN VANN");
+
     }
-    
+
+
 }
 
-knapp.addEventListener("click", function (){
-myscore = 0;
-score.innerHTML = myname + " har " + myscore + " antal poäng";
-computerscore = 0;
-cs.innerHTML = "Motståndare har " + computerscore + " antal poäng";
+knapp.addEventListener("click", function () {
+    myscore = 0;
+    score.innerHTML = myname + " har " + myscore + " antal poäng";
+    computerscore = 0;
+    cs.innerHTML = "Motståndare har " + computerscore + " antal poäng";
 })
